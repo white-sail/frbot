000b5cfe915abb1a4e18c27ce51d879448d61864
devsamadjon <esamadjon1@gmail.com>
2025-04-23 23:06:48 +0500

    Yangilangan kod2221

--- bot.js
+++ bot.js
@@ -10,6 +10,17 @@ require('./handlers/actions')(bot);
 bot.launch();
 console.log('Bot ishga tushdi 🚀');
 
+// 👇 HTTP serverni ishga tushuramiz (Render uchun muhim)
+const http = require('http');
+const PORT = process.env.PORT || 3000;
+
+http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Telegram bot ishga tushdi 🚀');
+}).listen(PORT, () => {
+  console.log(`HTTP server ${PORT} portda ishlayapti`);
+});
+
 // Graceful shutdown
 process.once('SIGINT', () => bot.stop('SIGINT'));
 process.once('SIGTERM', () => bot.stop('SIGTERM'));
