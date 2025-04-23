84da736af52ee766d062229fea3f2aa7fb0db5ac
devsamadjon <esamadjon1@gmail.com>
2025-04-23 15:07:38 +0500

    Qo'shimcha funksiyalar qo‘shildi

--- handlers/commands.js
+++ handlers/commands.js
@@ -2,5 +2,8 @@ module.exports = function (bot) {
     bot.start((ctx) => {
       ctx.reply('Salom, hush kelibsiz!');
     });
+    bot.salom((ctx)=>{
+        ctx.reply("salom hammaga")
+    })
   };
   
